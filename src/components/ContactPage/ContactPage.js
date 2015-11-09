579cf33165438fb769566adefe834c7f2c3819d6
tomadamec <tomadamec@users.noreply.github.com>
2015-11-09 18:33:31 +0100

    LinkTableByScopePlace - beginning

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -4,9 +4,11 @@ import React, { PropTypes, Component } from 'react';
 import styles from './ContactPage.css';
 import withStyles from '../../decorators/withStyles';
 
-import { Segment, Button, Input, Header, IconButton } from '../SEUI/elements';
+import { Segment, Button, Input, Header, IconButton, Icon, PopupButton } from '../SEUI/elements';
 import { Popup, Modal } from '../SEUI/modules';
-import { Form, Fields, Field } from '../SEUI/collections';
+import { Form, Fields, Field, Table } from '../SEUI/collections';
+
+import LinkTableByScopePlace from '../LinkTableByScopePlace/LinkTableByScopePlace';
 
 @withStyles(styles)
 class ContactPage extends Component {
@@ -23,45 +25,52 @@ class ContactPage extends Component {
 		};
 	}
 	
-	onMouseEnter() {
+	onMouseEnterX() {
 		this.setState({
 				show: true
 		});
   }
 
-	onMouseLeave() {
+	onMouseLeaveX() {
 		this.setState({
 				show: false
 		});
 	}
 
+
+
   render() {
     const title = 'Contact Us';
     this.context.onSetTitle(title);
-    return (
+   return (
       <div className="ContactPage">
         <div className="ContactPage-container">
-          <h1>{title}</h1>
-          <p>...</p>
 					<Header dividing tag="h1" key="popokatepetl">
 						Header
 					</Header>
 					<IconButton 
 						name="heart"
-						onMouseEnter={this.onMouseEnter.bind(this)}
-            onMouseLeave={this.onMouseLeave.bind(this)}
+						onMouseEnter={this.onMouseEnterX.bind(this)}
+            onMouseLeave={this.onMouseLeaveX.bind(this)}
 					>
 						It's alive!
 						<Popup active={this.state.show}>Very much so.</Popup>
 					</IconButton>
-					<IconButton 
-						name="idea"
-						onMouseEnter={this.onMouseEnter.bind(this)}
-            onMouseLeave={this.onMouseLeave.bind(this)}
+					<Button 
+						
 					>
+						<Icon name="idea"/>
 						I know!
-						<Popup active={this.state.show}>I am clever like that.</Popup>
-					</IconButton>
+						<Popup>I am clever like that.</Popup>
+					</Button>
+					
+					
+					
+					<LinkTableByScopePlace/>
+					
+					
+					
+				
         </div>
       </div>
     );
--- src/components/LinkTableByScopePlace/LinkTableByScopePlace.js
+++ src/components/LinkTableByScopePlace/LinkTableByScopePlace.js
@@ -1,4 +1,410 @@
 import React, { PropTypes, Component } from 'react';
+import styles from './LinkTableByScopePlace.css';
+import withStyles from '../../decorators/withStyles';
+
+import { Segment, Button, Input, Header, IconButton, Icon, PopupButton } from '../SEUI/elements';
+import { Popup, Modal } from '../SEUI/modules';
+import { Form, Fields, Field, Table } from '../SEUI/collections';
+
+@withStyles(styles)
+class LinkTableByScopePlace extends Component {
+
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired,
+  };
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			example: "Nothing is happening.",
+ 			auLevels: [
+				{
+					key: 1,
+					name: "AOI"
+				},
+				{
+					key: 2,
+					name: "Core City x Outer Urban Zone"
+				},
+				{
+					key: 3,
+					name: "GADM2"
+				},
+				{
+					key: 4,
+					name: "GADM3"
+				},
+				{
+					key: 5,
+					name: "GADM4"
+				}
+			],
+			auAttSets: [
+			{ 
+				key: 352,
+				name: "Land Cover classes L3", 
+				levels: [
+					{
+						key: 1,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "none"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					},
+					{
+						key: 2,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					},
+					{
+						key: 3,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "none"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "selected"
+							}
+						]
+					},
+					{
+						key: 4,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "none"
+							}
+						]
+					},
+					{
+						key: 5,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "none"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					}
+				]
+			}, // Attset
+			{ 
+				key: 623,
+				name: "Aggregated LC Classes Formation", 
+				levels: [
+					{
+						key: 1,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					},
+					{
+						key: 2,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "selected"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "selected"
+							}
+						]
+					},
+					{
+						key: 3,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "selected"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "selected"
+							}
+						]
+					},
+					{
+						key: 4,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "selected"
+							}
+						]
+					},
+					{
+						key: 5,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "selected"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					}
+				]
+			}, // Attset
+			{ 
+				key: 18,
+				name: "Populations1", 
+				levels: [
+					{
+						key: 1,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "selected"
+							}
+						]
+					},
+					{
+						key: 2,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					},
+					{
+						key: 3,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "only"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "only"
+							}
+						]
+					},
+					{
+						key: 4,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "none"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "none"
+							}
+						]
+					},
+					{
+						key: 5,
+						periods: [
+							{
+								key: 1,
+								name: "2000",
+								data: "none"
+							},
+							{
+								key: 2,
+								name: "2010",
+								data: "none"
+							}
+						]
+					}
+				]
+			} // Attset
+		]
+		};
+	}
+
+  openScreenExample(idAttSet,idAULevel) {
+		this.state.example = "Clicked on " + idAttSet + ", " + idAULevel;
+		this.forceUpdate();
+		/*alert("hey, " + idAttSet);*/
+	}
+
+	render() {
+    
+  	var thisComponent = this;
+		
+		var auLevelsInsert = this.state.auLevels.map(function (auLevel) {
+      return (
+				<td className="selectable">
+					<a href="#">
+						{auLevel.name}
+					</a>
+				</td>
+      );
+    });
+		
+		
+		var auAttSetsInsert = this.state.auAttSets.map(function (auAttSet) {
+      
+			var auSingleAttSetInsert = auAttSet.levels.map(function (auASLevel) {
+				var dataNoneCount = 0;
+				var tdClassName = "selectable";
+				var auASLPeriodsInsert = auASLevel.periods.map(function (period) {
+					var periodClassName = "data-" + period.data;
+					var iconName = "check circle";
+					if(period.data=="none"){
+						iconName="radio";
+						++dataNoneCount;
+					}
+					else if(period.data=="selected"){
+						iconName="check circle outline"
+					}
+					
+					return (
+						<span className={periodClassName}>
+							<Icon name={iconName}/>
+							{period.name}
+							<br/>
+						</span>
+					);
+				});
+				
+				if(dataNoneCount==0){
+					tdClassName += " positive";
+				}
+				else if(dataNoneCount==auASLevel.periods.length){
+					tdClassName += " negative";
+				}
+				else {
+					tdClassName += " warning";
+				}
+				
+				return (
+					<td className={tdClassName}>
+						<a 
+							href="#" 
+							onClick={thisComponent.openScreenExample.bind(
+									thisComponent,
+									auAttSet.key,
+									auASLevel.key
+								)}
+						>
+							{auASLPeriodsInsert}
+						</a>
+					</td>
+				);
+			});
+			
+			return (
+				<tr>
+					<td className="selectable">
+						<a href="#">
+							{auAttSet.name}
+						</a>
+					</td>
+					{auSingleAttSetInsert}
+				</tr>
+      );
+    });
+		
+		return (
+		 			
+		/* -> tabs - reference periods */
+		<div>
+			<br/>
+			<p>{this.state.example}</p>
+			
+		<Table celled className="LinkTable ByScopePlace fixed">
+			<thead>
+				<tr>
+					<th>Attribute Set</th>
+					<th colSpan={this.state.auLevels.length}>
+						Analytical units level
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td></td>
+					{auLevelsInsert}
+				</tr>
+				{auAttSetsInsert}
+			</tbody>
+		</Table>
+		
+		</div>
+    );
+  }
+
+}
+
+export default LinkTableByScopePlace;
+=======
 //import styles from './PageAnalysis.css';
 //import withStyles from '../../decorators/withStyles';
 
@@ -14,3 +420,4 @@ class LinkTableByScopePlace extends Component{
 }
 
 export default LinkTableByScopePlace
+>>>>>>> origin/improveAdmin
