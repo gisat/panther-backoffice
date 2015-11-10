03d4dd54507c6a1041c90115c6359bea50c3c9a0
jveselka <jonas.jonas.jonas@gmail.com>
2015-11-10 17:40:26 +0100

    config, details

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 /*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
 export const googleAnalyticsId = 'UA-XXXXX-X';
-export const publicPath = "";
+export const publicPath = "/puma-backoffice"; ///// don't forget leading slash
 
