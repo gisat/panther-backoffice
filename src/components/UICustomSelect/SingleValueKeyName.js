54717ddba0a4fd27db01dea023e5690a3cf5d712
tmdmc <tmdmc@users.noreply.github.com>
2016-01-07 10:45:29 +0100

    custom select options: key-name components: now with the components

---
+++ src/components/UICustomSelect/SingleValueKeyName.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var React = require('react');
+var classes = require('classnames');
+
+var SingleValue = React.createClass({
+	displayName: 'SingleValue',
+
+	propTypes: {
+		placeholder: React.PropTypes.string, // this is default value provided by React-Select based component
+		value: React.PropTypes.object // selected option
+	},
+	renderValue (option) {
+		return (
+			<div>
+				<span className="option-id">{option.key}</span>
+				<span>{option.name}</span>
+			</div>
+		);
+	},
+	render: function render() {
+		var classNames = classes('Select-placeholder', "UICustomSelect-value-keyname", this.props.value && this.props.value.className);
+		var label = this.props.value ? this.renderValue(this.props.value) : this.props.placeholder;
+		return React.createElement(
+			'div',
+			{
+				className: classNames,
+				style: this.props.value && this.props.value.style,
+				title: this.props.value && this.props.value.title
+			},
+			label
+		);
+	}
+});
+
+module.exports = SingleValue;
