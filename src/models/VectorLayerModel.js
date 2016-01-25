e9bf2c6f31cee522f0976887918970727fa2b6f1
tmdmc <tmdmc@users.noreply.github.com>
2016-01-25 13:59:23 +0100

    temp vector layers

---
+++ src/models/VectorLayerModel.js
@@ -0,0 +1,82 @@
+import Model from './Model';
+import UserStore from '../stores/UserStore';
+import LayerGroupStore from '../stores/LayerGroupStore';
+import StyleStore from '../stores/StyleStore';
+import TopicStore from '../stores/TopicStore';
+
+
+class VectorLayerModel extends Model {
+
+	constructor(options) {
+		super(options);
+
+	}
+
+	data() {
+		return {
+			key: {
+				serverName: '_id' //number
+			},
+			name: {
+				serverName: 'name' //string
+			},
+			active: {
+				serverName: 'active' //boolean
+			},
+			changed: {
+				serverName: 'changed', //date
+				transformForLocal: function () {
+					this.transformDate.apply(this, arguments)
+				}.bind(this)
+			},
+			changedBy: {
+				serverName: 'changedBy', //id
+				transformForLocal: function (options) {
+					return UserStore.getById(options)
+				},
+				isPromise: true
+			},
+			created: {
+				serverName: 'created', //date
+				transformForLocal: function () {
+					this.transformDate.apply(this, arguments)
+				}.bind(this)
+			},
+			createdBy: {
+				serverName: 'createdBy', //id
+				transformForLocal: function (options) {
+					return UserStore.getById(options)
+				},
+				isPromise: true
+			},
+			layerType: {
+				serverName: 'layerType' // raster / vector / au ?unnecessary in this direction?
+			},
+			layerGroup: {
+				serverName: 'layerGroup', //id
+				transformForLocal: function (options) {
+					return LayerGroupStore.getById(options)
+				},
+				isPromise: true
+			},
+			styles: {
+				serverName: 'symbologies', //ids
+				transformForLocal: function (options) {
+					return StyleStore.getFiltered(options)
+				},
+				isPromise: true,
+				isArray: true
+			},
+			topic: {
+				serverName: 'topic', //id
+				transformForLocal: function (options) {
+					return TopicStore.getById(options)
+				},
+				isPromise: true
+			}
+		};
+	}
+
+}
+
+export default VectorLayerModel;
--- src/stores/VectorLayerStore.js
+++ src/stores/VectorLayerStore.js
@@ -1,8 +1,43 @@
-import FauxStore from './FauxStore';
+import Store from './Store';
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import ActionTypes from '../constants/ActionTypes';
+import VectorLayerModel from '../models/VectorLayerModel';
 
-class VectorLayerStore extends FauxStore {
+class VectorLayerStore extends Store {
 
+	getApiUrl(){
+		return "/rest/areatemplate";
+	}
+	getInstance(data){
+		// for now we have non-raster layers (vector + AU levels)
+		if(!data.justVisualization) {
+			return new VectorLayerModel(data);
+		}
+		else {
+			return null;
+		}
+	}
 }
+
 let storeInstance = new VectorLayerStore();
+
+storeInstance.dispatchToken = AppDispatcher.register(action => {
+
+	switch(action.type) {
+		//case ActionTypes.APP_INITIALIZED:
+		//	reset();
+		//	break;
+		//case ActionTypes.LAYER_ADD:
+		//	//appState.page = action.page;
+		//	//appState.path = action.path;
+		//	console.log("Action: LAYER_ADD");
+		//	break;
+		default:
+			return;
+	}
+
+	storeInstance.emitChange();
+
+});
 
 export default storeInstance;
