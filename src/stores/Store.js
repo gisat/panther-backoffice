4bf4a60ea07aaa8b7e713badc5e7a16548ee5319
tmdmc <tmdmc@users.noreply.github.com>
2016-01-27 17:19:15 +0100

    Store requests common logic united + cleanup

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -12,6 +12,14 @@ import { publicPath } from '../config';
 
 class Store extends EventEmitter {
 
+	constructor() {
+		super();
+		this._models = this.load(); //mozna ne tady ale primo do filtru atd. lazy load
+		this._models.then(function(models){
+			console.log("Store > constructor > _models",models);
+		});
+	}
+
 	/**
 	 * To be overridden
 	 */
@@ -24,38 +32,95 @@ class Store extends EventEmitter {
 	}
 
 	getInstance(obj){
-		//console.err("getModel not overridden");
+		//console.err("getInstance not overridden");
 		return {};
 	}
 
-	constructor() {
-		super();
-		this._models = this.load(); //mozna ne tady ale primo do filtru atd. lazy load
-		this._models.then(function(models){
-			console.log("Store > constructor > _models",models);
-		});
-	}
 
 	emitChange() {
 		this.emit(EventTypes.STORE_CHANGE);
 	}
 
-	/**
-	 * To be overridden
-	 */
-	load(){
-		// todo ajax s parametrem
+	addChangeListener(callback) {
+		this.on(EventTypes.STORE_CHANGE, callback);
+	}
+
+	removeChangeListener(callback) {
+		this.removeListener(EventTypes.STORE_CHANGE, callback);
+	}
+
+	addResponseListener(callback) {
+		this.on(EventTypes.OBJECT_CREATED, callback);
+		//this.on(EventTypes.OBJECT_CREATE_FAILED, callback); //etc
+	}
+
+	removeResponseListener(callback) {
+		this.removeListener(EventTypes.OBJECT_CREATED, callback);
+	}
+
+
+	///**
+	// * To be overridden
+	// */
+	//load(){
+	//	var me = this;
+	//	return new Promise(function (resolve, reject) {
+	//		var url = path.resolve(publicPath, "api-proxy");
+	//		superagent
+	//			.post(url)
+	//			.send({apiUrl: me.getApiUrl()})
+	//			.send({method: me.getApiLoadMethod()})
+	//			.send({ssid: "5oymzxv5yigf4n6dp2nda2vgu6ernils"})
+	//			.send({sessionid: "kzgfcqe0a26jefi5c942hm7azef5od90"})
+	//			.send({csrftoken: "VNsl5vgDXeEDRl3J4NgNt8BjBfmHgD9b"})
+	//			//.send({necoDalsiho: "dmewidoiwjefowng"})
+	//			.end(function(err, res){
+	//				if(err || typeof res == 'undefined'){
+	//					reject(err);
+	//					return;
+	//				}
+	//				var ret = [];
+	//				var responseJson = JSON.parse(res.text);
+	//				if(typeof responseJson.data == 'undefined'){
+	//					reject("no data attribute");
+	//					return;
+	//				}
+	//				for(let obj of responseJson.data){
+	//					let instance = me.getInstance(obj);
+	//					if(instance){
+	//						ret.push(instance);
+	//					}
+	//					//ret.push(new DataLayerModel(obj));
+	//				}
+	//				resolve(ret);
+	//			});
+	//	});
+	//}
+	load() {
+		var method = this.getApiLoadMethod();
+		return this.request(method);
+	}
+
+	create(object) {
+		var method = "POST";
+		return this.request(method, object);
+	}
+
+	request(method,object){
+		if (!object) {
+			object = {};
+		}
 		var me = this;
 		return new Promise(function (resolve, reject) {
 			var url = path.resolve(publicPath, "api-proxy");
 			superagent
 				.post(url)
 				.send({apiUrl: me.getApiUrl()})
-				.send({method: me.getApiLoadMethod()})
+				.send({method: method})
 				.send({ssid: "5oymzxv5yigf4n6dp2nda2vgu6ernils"})
 				.send({sessionid: "kzgfcqe0a26jefi5c942hm7azef5od90"})
 				.send({csrftoken: "VNsl5vgDXeEDRl3J4NgNt8BjBfmHgD9b"})
-				//.send({necoDalsiho: "dmewidoiwjefowng"})
+				//.send({data: object})
 				.end(function(err, res){
 					if(err || typeof res == 'undefined'){
 						reject(err);
@@ -77,22 +142,6 @@ class Store extends EventEmitter {
 					resolve(ret);
 				});
 		});
-	}
-
-	addChangeListener(callback) {
-		this.on(EventTypes.STORE_CHANGE, callback);
-	}
-
-	removeChangeListener(callback) {
-		this.removeListener(EventTypes.STORE_CHANGE, callback);
-	}
-
-	addResponseListener(callback) {
-		this.on(EventTypes.OBJECT_CREATED, callback);
-	}
-
-	removeResponseListener(callback) {
-		this.removeListener(EventTypes.OBJECT_CREATED, callback);
 	}
 
 	getFiltered(options){
