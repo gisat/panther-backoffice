ea9ef2a00cfc0deb62f97e118a33bf98be8e8d5e
tmdmc <tmdmc@users.noreply.github.com>
2016-02-10 10:31:36 +0100

    Rendering column table 2

--- src/components/elements/ColumnTableRow/ColumnTableRow.js
+++ src/components/elements/ColumnTableRow/ColumnTableRow.js
@@ -5,6 +5,10 @@ import React, { PropTypes, Component } from 'react';
 import Select from 'react-select';
 import UIObjectSelect from '../../atoms/UIObjectSelect';
 
+import OptionDestination from '../../atoms/UICustomSelect/OptionDestination';
+import SingleValueDestination from '../../atoms/UICustomSelect/SingleValueDestination';
+
+
 //@withStyles(styles)
 class ColumnTableRow extends Component {
 
@@ -20,14 +24,34 @@ class ColumnTableRow extends Component {
 	};
 
 	render() {
+
+		var periodSelect = "";
+
+		if(!this.props.destinationValue || (this.props.destinationValue[0]!="I" && this.props.destinationValue[0]!="N" && this.props.destinationValue[0]!="P")) {
+			periodSelect = (
+				<UIObjectSelect
+					//onChange={this.props.onChangePeriod.bind(this,this.props.columnName)}
+					//loadOptions={this.getPlaces}
+					multi
+					options={this.props.periods}
+					valueKey="key"
+					labelKey="name"
+					//inputProps={selectInputProps}
+					value={this.props.selectedPeriods}
+				/>
+			);
+		}
+
 		return(
 			<tr>
 				<td className="header">{this.props.columnName}</td>
 				<td className="allowOverflow resetui">
 					<Select
-						//onChange={this.onChangeDestination.bind(this)}
+						//onChange={this.onChangeDestination.bind(this,this.props.columnName)}
 						//loadOptions={this.getPlaces}
 						options={this.props.destinations}
+						optionComponent={OptionDestination}
+						singleValueComponent={SingleValueDestination}
 						valueKey="key"
 						labelKey="name"
 						//inputProps={selectInputProps}
@@ -35,16 +59,7 @@ class ColumnTableRow extends Component {
 					/>
 				</td>
 				<td className="allowOverflow resetui">
-					<UIObjectSelect
-						//onChange={this.onChangePeriod.bind(this)}
-						//loadOptions={this.getPlaces}
-						multi
-						options={this.props.periods}
-						valueKey="key"
-						labelKey="name"
-						//inputProps={selectInputProps}
-						value={this.props.selectedPeriods}
-					/>
+					{periodSelect}
 				</td>
 			</tr>
 		);
