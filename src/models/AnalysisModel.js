dc51a107134ac2fbad030355c2afc94f233ed327
jveselka <jonas.jonas.jonas@gmail.com>
2016-03-01 23:06:46 +0100

    more fields in model

--- src/models/AnalysisModel.js
+++ src/models/AnalysisModel.js
@@ -1,16 +1,21 @@
 import Model from './Model';
 import UserStore from '../stores/UserStore';
+import TopicStore from '../stores/TopicStore';
+import GeneralLayerStore from '../stores/GeneralLayerStore';
+import AttributeSetStore from '../stores/AttributeSetStore';
+
+import AttributeStore from '../stores/AttributeStore';
 
 /*
- areaTemplate	Feature Layer Template (Spatial)
- attributeMap	Attribute Grid (FID, Spatial)
- attributeSet	Result Attribute Set (Spatial)
- attributeSets	Attribute Sets (FID, Math)
- groupAttribute	Group Attribute (Spatial)
- groupAttributeSet	Group AttributeSet (Spatial)
-		 name	Name (all)
- topics	Topics for filtering Attribute Sets (all)
- type	Type (Spatial aggregation, Math, FID aggregation)
+		areaTemplate	Feature Layer Template (Spatial)
+attributeMap	Attribute Grid (FID, Spatial)
+		attributeSet	Result Attribute Set (Spatial)
+attributeSets	Attribute Sets (FID, Math)
+groupAttribute	Group Attribute (Spatial)
+groupAttributeSet	Group AttributeSet (Spatial)
+		name	Name (all)
+ 		topics	Topics for filtering Attribute Sets (all)
+		type	Type (Spatial aggregation, Math, FID aggregation)
  */
 
 
@@ -25,6 +30,42 @@ class AnalysisModel extends Model {
 			name: {
 				serverName: 'name', //string
 				sendToServer: true
+			},
+			topics: {
+				serverName: 'topics', //ids
+				sendToServer: true,
+				transformForLocal: function (data) {
+					return TopicStore.getFiltered({key: data})
+				},
+				transformForServer: this.getKeys,
+				isPromise: true,
+				isArray: true
+			},
+			analysisType: {
+				serverName: 'type', // spatial, fid, math
+				sendToServer: true,
+				transformForLocal: function (data) {
+					//if(!data) { data = ""; } todo default value?
+					return data;
+				}
+			},
+			layerObject: {
+				serverName: 'areaTemplate', //id
+				sendToServer: true,
+				transformForLocal: function (data) {
+					return GeneralLayerStore.getById(data)
+				},
+				transformForServer: this.getKey,
+				isPromise: true
+			},
+			attributeSet: {
+				serverName: 'attributeSet', //id
+				sendToServer: true,
+				transformForLocal: function (data) {
+					return AttributeSetStore.getById(data)
+				},
+				transformForServer: this.getKey,
+				isPromise: true
 			},
 			changed: {
 				serverName: 'changed', //date
