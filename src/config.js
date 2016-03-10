6d6cbb96551d5705c67e3c5b27c74ad900a6939f
tmdmc <tmdmc@users.noreply.github.com>
2016-03-10 10:55:31 +0100

    :cookie: new temp cookies

--- src/config.js
+++ src/config.js
@@ -8,6 +8,6 @@ export const apiHost = "37.205.9.78";
 export const apiPath = "/tool/";
 
 
-export const tempSsid = "et0gj7gpfx1yzwan7ny8bcb961x017h3";
+export const tempSsid = "axssclakw1btrb2286erdniw14q5k7dn";
 export const tempSessionid = "3nbg1bhc6ticj1kkkwyx1j5kg4njiz50";
-export const tempCsrftoken = "109rPfpkeCQRH71OuuFIRBxQ8RDRrZkT";
+export const tempCsrftoken = "W9zb8gJwOcI9qeqbpQezsuFieLIAw1yv";
