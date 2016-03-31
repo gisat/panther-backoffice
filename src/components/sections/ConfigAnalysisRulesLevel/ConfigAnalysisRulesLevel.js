ed344c6250f9a200bffabbd11df07d32e774f8fe
tmdmc <tmdmc@users.noreply.github.com>
2016-03-31 21:43:15 +0200

    Analyses: config table for level

--- src/components/sections/ConfigAnalysisRulesLevel/ConfigAnalysisRulesLevel.js
+++ src/components/sections/ConfigAnalysisRulesLevel/ConfigAnalysisRulesLevel.js
@@ -18,146 +18,65 @@ import ScreenMetadataObject from '../../screens/ScreenMetadataObject';
 
 import ListenerHandler from '../../../core/ListenerHandler';
 
+const OPERATIONS = [
+	{ key: "sum", name: "SUM" },
+	{ key: "avgarea", name: "AVERAGE, weighted by area/length" },
+	{ key: "avgattr", name: "AVERAGE, weighted by attribute" }
+];
 
-var initialState = {
-	place: null,
-	valueActive: false,
-	valueName: "",
-	valueBoundingBox: "",
-	valueScope: []
-};
-
-
-class ConfigAnalysisRulesLevel extends Component{
+class ConfigAnalysisRulesLevel extends Component {
 
 	static propTypes = {
-		disabled: React.PropTypes.bool,
-		selectorValue: React.PropTypes.any
+		disabled: PropTypes.bool,
+		analysis: PropTypes.object.isRequired
 	};
 
 	static defaultProps = {
-		disabled: false,
-		selectorValue: null
+		disabled: false
 	};
-
-	static contextTypes = {
-		setStateFromStores: PropTypes.func.isRequired,
-		onInteraction: PropTypes.func.isRequired,
-		screenSetKey: PropTypes.string.isRequired
-	};
-
-	constructor(props) {
-		super(props);
-		this.state = utils.deepClone(initialState);
-
-		// this.changeListener = new ListenerHandler(this, this._onStoreChange, 'addChangeListener', 'removeChangeListener');
-		// this.responseListener = new ListenerHandler(this, this._onStoreResponse, 'addResponseListener', 'removeResponseListener');
-	}
-
-	// store2state(props) {
-	// 	return {
-	// 		place: PlaceStore.getById(props.selectorValue),
-	// 		scopes: ScopeStore.getAll()
-	// 	};
-	// }
-
-	// setStateFromStores(props,keys) {
-	// 	if(!props){
-	// 		props = this.props;
-	// 	}
-	// 	if(props.selectorValue) {
-	// 		var thisComponent = this;
-	// 		let store2state = this.store2state(props);
-	// 		this.context.setStateFromStores.call(this, store2state, keys);
-	// 		// if stores changed, overrides user input - todo fix
-	//
-	// 		if(!keys || keys.indexOf("place")!=-1) {
-	// 			store2state.place.then(function (place) {
-	// 				let newState = {
-	// 					valueActive: place.active,
-	// 					valueName: place.name,
-	// 					valueBoundingBox: place.boundingBox,
-	// 					valueScope: place.scope ? [place.scope.key] : []
-	// 				};
-	// 				newState.savedState = utils.deepClone(newState);
-	// 				thisComponent.setState(newState);
-	// 			});
-	// 		}
-	// 	}
-	// }
-
-
-
-	// componentDidMount() {
-	// 	this.changeListener.add(PlaceStore,["place"]);
-	// 	this.changeListener.add(ScopeStore,["scopes"]);
-	// 	this.responseListener.add(ScopeStore);
-	//
-	// 	this.setStateFromStores();
-	// }
-	//
-	// componentWillUnmount() {
-	// 	this.changeListener.clean();
-	// 	this.responseListener.clean();
-	// }
-
-	componentWillReceiveProps(newProps) {
-
-	}
-
-
 
 	render() {
 
+		let ret = null;
+		if (this.props.analysis) {
 
+			console.log("analysis",this.props.analysis);
 
-		return (
-		<Table celled className="fixed">
-			<thead>
-			<tr>
-				<th>Result Attribute</th>
-				<th>Operation</th>
-				<th>Filter</th>
-			</tr>
-			</thead>
-			<tbody>
+			let rowsInsert = [];
+			for (let record of this.props.analysis.attributeMap) {
 
-			<tr>
-				<td>Continuous Urban Fabric (S.L. > 80%)</td>
-				<td>SUM (area/length)</td>
-				<td>Status code: 111</td>
-			</tr>
+				let operation = _.findWhere(OPERATIONS,{key: record.operationType});
 
-			<tr>
-				<td>Discontinuous High Dense Urban Fabric (S.L. 50% - 80%)</td>
-				<td>SUM (area/length)</td>
-				<td>Status code: 112</td>
-			</tr>
+				let row = (
+					<tr>
+						<td>{record.attribute.name}</td>
+						<td>{operation.name}</td>
+					</tr>
+				);
 
-			<tr>
-				<td>Discontinuous Low Dense Urban Fabric (S.L.: 10% - 50%)</td>
-				<td>SUM (area/length)</td>
-				<td>Status code: 113</td>
-			</tr>
+				rowsInsert.push(row);
 
-			<tr>
-				<td>Industrial, Commercial and Transport Units</td>
-				<td>SUM (area/length)</td>
-				<td>Status code: 120, 121</td>
-			</tr>
+			}
 
-			<tr>
-				<td>Construction sites</td>
-				<td>SUM (area/length)</td>
-				<td>Status code: 130</td>
-			</tr>
+			ret = (
+				<Table celled className="fixed">
+					<thead>
+					<tr>
+						<th>Result Attribute</th>
+						<th>Operation</th>
+					</tr>
+					</thead>
+					<tbody>
 
-			</tbody>
-		</Table>
+					{rowsInsert}
 
+					</tbody>
+				</Table>
+			);
 
+		}
 
-		);
+		return ret;
 
 	}
 }
