368f90a40466b3a7b6c4e237e35ab9d7a44de853
tmdmc <tmdmc@users.noreply.github.com>
2016-04-01 18:33:25 +0200

    Help: Index draft

--- src/components/screens/ScreenHelpIndex/ScreenHelpIndex.js
+++ src/components/screens/ScreenHelpIndex/ScreenHelpIndex.js
@@ -22,12 +22,71 @@ class ScreenHelpIndex extends Component {
 	//	ActionCreator.createOpenScreen(screenName,this.context.screenSetKey, options);
 	//}
 
+	onClick() {
+		console.log("click");
+	}
+
 	render() {
 
 		return (
 			<div>
-				<div className="screen-content-only"><div>
+				<div className="screen-content-only help-index"><div>
 					<h2>Help</h2>
+
+					<h3>Topics</h3>
+
+					<a
+						className="help-link"
+						onClick={this.onClick}
+					>
+						<span>
+							PUMA system architecture
+						</span>
+					</a>
+					<a className="help-link">
+						<span>
+							Metadata structures
+						</span>
+						<span className="description">
+							Metadata types used in PUMA to describe uploaded data
+						</span>
+					</a>
+
+					<a className="help-link">
+						<span>
+							Back Office interface
+						</span>
+						<span className="description">
+							Sections and controls in Back Office
+						</span>
+					</a>
+					<a className="help-link">
+						<span>
+							Keyboard shorcuts
+						</span>
+					</a>
+
+					<h3>Use cases</h3>
+
+					<a className="help-link">
+						<span>
+							Adding a new layer
+						</span>
+						<span className="description">
+							Uploading, describing and displaying layers
+						</span>
+					</a>
+					<a className="help-link">
+						<span>
+							Creating a new analysis
+						</span>
+					</a>
+					<a className="help-link">
+						<span>
+							Managing a place
+						</span>
+					</a>
+
 				</div></div>
 			</div>
 		);
