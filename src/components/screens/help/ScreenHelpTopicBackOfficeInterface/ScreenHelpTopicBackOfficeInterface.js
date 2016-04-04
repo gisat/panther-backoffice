6e8d1c7ef1e18710db6ee13706c5ec28ceb19d25
tmdmc <tmdmc@users.noreply.github.com>
2016-04-04 17:20:14 +0200

    Help: BO interface - 2

--- src/components/screens/help/ScreenHelpTopicBackOfficeInterface/ScreenHelpTopicBackOfficeInterface.js
+++ src/components/screens/help/ScreenHelpTopicBackOfficeInterface/ScreenHelpTopicBackOfficeInterface.js
@@ -49,7 +49,7 @@ class ScreenHelpTopicBackOfficeInterface extends Component {
 				<div className="screen-content-only help-page"><div>
 					<h2>Help: Back Office interface</h2>
 
-					<p>The backoffice interface is built around main tasks while allowing for the required subtasks to be completed in context of the main task.</p>
+					<p>The backoffice interface is divided into sections built around main tasks while allowing for the required subtasks to be completed in context of the main task.</p>
 
 					<img
 						className="image-fit"
@@ -58,6 +58,7 @@ class ScreenHelpTopicBackOfficeInterface extends Component {
 					<div className="image-description">
 						Main task: Managing a place (left panel), subtask: setting data source (right panel)
 					</div>
+
 
 					<div className="section-header">
 						<h3>Sections</h3>
@@ -93,8 +94,11 @@ class ScreenHelpTopicBackOfficeInterface extends Component {
 							/>
 						</div>
 						<h3>Data layers</h3>
-						<p>Manage a data layer. Describe the layer and all information included in it and connect it to a scope, place and topics.<br/>
-							<i>Both Places and Data layers internally serve the same purpose, but approach it from different angles in acord with the administrator’s current task.</i></p>
+						<p>Manage a data layer. Describe the layer and all information included in it and connect it to a scope, place and topics.</p>
+					</div>
+
+					<div className="help-bo-section">
+						<p><i>Both Places and Data layers internally serve the same purpose, but approach it from different angles in acord with the administrator’s current task.</i></p>
 					</div>
 
 					<div className="help-bo-section">
@@ -116,8 +120,38 @@ class ScreenHelpTopicBackOfficeInterface extends Component {
 							/>
 						</div>
 						<h3>Metadata</h3>
-						<p>Manage all metadata structures in the Back Office.</p>
+						<p>Manage all metadata structures in Back Office.</p>
 					</div>
+
+
+					<div className="section-header">
+						<h3>Controls</h3>
+					</div>
+
+					<h4>Panels</h4>
+					<p>To provide flexible use, components of Back Office are placed in panels, which can be opened from various contexts.<br/>
+					Every control opening a panel when activated is marked by a &raquo; symbol. An open panel can be closed by its close (&times;) button or retracted (&rsaquo;). You can return to a retracted panel by opening it by clicking anywhere on it or using the open (&lsaquo;) button.</p>
+
+					<h4>Data objects</h4>
+					<p>Metadata objects are displayed as below, with their ID and name. Clicking an object opens panel with object settings. Blue background indicates simple object (all its data are visible in the settings), gray background indicates a template (filled from data sources with different data in different contexts - places, periods, etc.).</p>
+
+					<a className="puma-item">
+						<span className="item-id">
+							1532
+						</span>
+						<span>
+							Local
+						</span>
+					</a>
+
+					<a className="puma-item template">
+						<span className="item-id">
+							652
+						</span>
+						<span>
+							Land cover
+						</span>
+					</a>
 
 				</div></div>
 			</div>
