1a11eb3893fe17aa8d67fab897c459f8c79b2ff8
jveselka <jonas.jonas.jonas@gmail.com>
2016-04-06 18:08:13 +0200

    removed GA tracking code from config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
-export const googleAnalyticsId = 'UA-43870130-2';
+export const googleAnalyticsId = 'UA-XXXXX-X'; // edit this on deploy
 export const publicPath = ""; ///// don't forget leading slash. like this: /backoffice
 export const serverPort = 5555;
 
