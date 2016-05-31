a790a8ee5185c643a345edb1c10e09eeb65584d4
tmdmc <tmdmc@users.noreply.github.com>
2016-05-31 17:38:00 +0200

    Style model: start

--- src/models/StyleModel.js
+++ src/models/StyleModel.js
@@ -1,6 +1,8 @@
 import Model from './Model';
 import UserStore from '../stores/UserStore';
 import TopicStore from '../stores/TopicStore';
+//import AttributeStore from '../stores/AttributeStore';
+//import AttributeSetStore from '../stores/AttributeSetStore';
 
 
 class StyleModel extends Model {
@@ -54,11 +56,92 @@ class StyleModel extends Model {
 				transformForServer: this.getKey,
 				isPromise: true
 			},
+			source: {
+				serverName: 'source', // string - definition/geoserver/?
+				sendToServer: true,
+				transformForLocal: function (data) {
+					if (data) {
+						return data;
+					} else {
+						return 'geoserver';
+					}
+				}
+			},
 			serverName: {
 				serverName: 'symbologyName', //string
 				sendToServer: true
+			},
+			definition: {
+				serverName: 'definition',
+				sendToServer: true,
+				isNested: true,
+				model: {
+					type: {
+						serverName: 'type', //string - polygon/line/point
+						sendToServer: true
+					},
+					filterAttribute: {
+						serverName: 'filterAttribute', //id
+						sendToServer: true,
+						//transformForLocal: function (data) {
+						//	return AttributeStore.getById(data);
+						//},
+						//transformForServer: this.getKey,
+						isPromise: true
+					},
+					filterAttributeSet: {
+						serverName: 'filterAttributeSet', //id
+						sendToServer: true,
+						//transformForLocal: function (data) {
+						//	return AttributeSetStore.getById(data);
+						//},
+						//transformForServer: this.getKey,
+						isPromise: true
+					},
+					rules: {
+						serverName: 'rules',
+						sendToServer: true,
+						isArrayOfNested: true,
+						model: {
+							name: {
+								serverName: 'name',
+								sendToServer: true
+							},
+							title: {
+								serverName: 'title',
+								sendToServer: true
+							},
+							filter: {
+								serverName: 'filter', //string - csv
+								sendToServer: true
+							},
+							appearance: {
+								serverName: 'appearance',
+								sendToServer: true,
+								isNested: true,
+								model: {
+									fillColour: {
+										serverName: 'fillColour', //hex
+										sendToServer: true
+									},
+									strokeColour: {
+										serverName: 'strokeColour', //hex
+										sendToServer: true
+									},
+									strokeWidth: {
+										serverName: 'strokeWidth', //number - 0.1
+										sendToServer: true
+									},
+									shapeSize: {
+										serverName: 'shapeSize', //number - 8
+										sendToServer: true
+									}
+								}
+							}
+						}
+					}
+				}
 			}
-			// todo origin? - geonode styles
 		};
 	}
 
