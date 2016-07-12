5d73c01d8f5dba5e3c3b3fb0ffccee3a1d2bd720
jveselka <jonas.jonas.jonas@gmail.com>
2016-07-12 15:51:28 +0200

    ConfigMetadataPeriod refactored to ControllerComponent extension

--- src/components/sections/ConfigMetadataPeriod/ConfigMetadataPeriod.js
+++ src/components/sections/ConfigMetadataPeriod/ConfigMetadataPeriod.js
@@ -1,114 +1,66 @@
 import React, { PropTypes, Component } from 'react';
-import PantherComponent from '../../common/PantherComponent';
+import ControllerComponent from '../../common/ControllerComponent';
 
 import utils from '../../../utils/utils';
 
-import { Input, Button } from '../../SEUI/elements';
-import { CheckboxFields, Checkbox } from '../../SEUI/modules';
+import { Input } from '../../SEUI/elements';
 import _ from 'underscore';
 import SaveButton from '../../atoms/SaveButton';
 
 import ObjectTypes, {Model} from '../../../constants/ObjectTypes';
 import ActionCreator from '../../../actions/ActionCreator';
-import PeriodStore from '../../../stores/PeriodStore';
+import ScopeModel from '../../../models/ScopeModel';
+import PeriodModel from '../../../models/PeriodModel';
 
-import ListenerHandler from '../../../core/ListenerHandler';
 import logger from '../../../core/Logger';
 
 var initialState = {
-	period: null,
 	valueActive: false,
 	valueName: ""
 };
 
 
-class ConfigMetadataPeriod extends PantherComponent{
+class ConfigMetadataPeriod extends ControllerComponent{
 
 	static propTypes = {
 		disabled: React.PropTypes.bool,
+		scope: PropTypes.instanceOf(ScopeModel),
+		store: PropTypes.shape({
+			periods: PropTypes.arrayOf(PropTypes.instanceOf(PeriodModel))
+		}).isRequired,
 		selectorValue: React.PropTypes.any
 	};
 
 	static defaultProps = {
 		disabled: false,
+		scope: null,
 		selectorValue: null
 	};
 
 	static contextTypes = {
-		onInteraction: PropTypes.func.isRequired
+		onInteraction: PropTypes.func.isRequired,
+		screenSetKey: PropTypes.string.isRequired
 	};
 
 	constructor(props) {
 		super(props);
-		this.state = utils.deepClone(initialState);
+		this.state.current = _.assign(this.state.current, utils.deepClone(initialState));
+		this.state.saved = utils.clone(this.state.current);
 	}
 
-	store2state(props) {
-		return {
-			period: PeriodStore.getById(props.selectorValue)
-		};
-	}
-
-	setStateFromStores(props,keys) {
-		if(!props){
-			props = this.props;
-		}
+	buildState(props){
+		props = props || this.props;
+		let nextState = {};
 		if(props.selectorValue) {
-			var thisComponent = this;
-			let store2state = this.store2state(props);
-			super.setStateFromStores(store2state, keys);
-			// if stores changed, overrides user input - todo fix
-
-			store2state.period.then(function(period) {
-				if(thisComponent.acceptChange) {
-					thisComponent.acceptChange = false;
-					if (thisComponent.mounted) {
-						thisComponent.setState({
-							valueActive: period.active,
-							valueName: period.name
-						});
-					}
-				}
-			});
+			let period = _.findWhere(props.store.periods, {key: props.selectorValue});
+			nextState = {
+				valueActive: period.active,
+				valueName: period.name
+			};
 		}
-
+		return nextState;
 	}
 
-	_onStoreChange(keys) {
-		logger.trace("ConfigMetadataPeriod# _onStoreChange(), Keys:", keys);
-		this.setStateFromStores(this.props,keys);
-	}
-
-	componentDidMount() {
-		super.componentDidMount();
-		this.changeListener.add(PeriodStore, ["period"]);
-
-		this.setStateFromStores();
-	}
-
-	componentWillReceiveProps(newProps) {
-		if(newProps.selectorValue!=this.props.selectorValue) {
-			this.acceptChange = true;
-			this.setStateFromStores(newProps);
-			this.updateStateHash(newProps);
-		}
-	}
-
-
-	/**
-	 * Check if state is the same as it was when loaded from stores
-	 * @returns {boolean}
-	 */
-	isStateUnchanged() {
-		var isIt = true;
-		if(this.state.period) {
-			isIt = (
-				this.state.valueActive == this.state.period.active &&
-				this.state.valueName == this.state.period.name
-			);
-		}
-		return isIt;
-	}
 
 	/**
 	 * Differentiate between states
@@ -121,32 +73,27 @@ class ConfigMetadataPeriod extends PantherComponent{
 		// todo hash influenced by screen/page instance / active screen (unique every time it is active)
 		this._stateHash = utils.stringHash(props.selectorValue);
 	}
-	getStateHash() {
-		if(!this._stateHash) {
-			this.updateStateHash();
-		}
-		return this._stateHash;
-	}
 
 	saveForm() {
 		super.saveForm();
 		var actionData = [], modelData = {};
-		_.assign(modelData, this.state.period);
-		modelData.active = this.state.valueActive;
-		modelData.name = this.state.valueName;
+		let period = _.findWhere(this.props.store.periods, {key: this.props.selectorValue});
+		_.assign(modelData, period);
+		// modelData.active = this.state.current.valueActive;
+		modelData.name = this.state.current.valueName;
 		let modelObj = new Model[ObjectTypes.PERIOD](modelData);
 		actionData.push({type:"update",model:modelObj});
 		ActionCreator.handleObjects(actionData,ObjectTypes.PERIOD);
 	}
 
 	onChangeActive() {
-		this.setState({
-			valueActive: !this.state.valueActive
+		this.setCurrentState({
+			valueActive: !this.state.current.valueActive
 		});
 	}
 
 	onChangeName(e) {
-		this.setState({
+		this.setCurrentState({
 			valueName: e.target.value
 		});
 	}
@@ -154,45 +101,49 @@ class ConfigMetadataPeriod extends PantherComponent{
 
 	render() {
 
+		let ret = null;
+
 		var saveButton = " ";
-		if (this.state.period) {
+		let period = _.findWhere(this.props.store.periods, {key: this.props.selectorValue});
+		if (period && this.state.built) {
 			saveButton = (
 				<SaveButton
-					saved={this.isStateUnchanged()}
+					saved={this.equalStates(this.state.current,this.state.saved)}
+					saving={this.state.saving}
 					className="save-button"
 					onClick={this.saveForm.bind(this)}
 				/>
 			);
-		}
+			// var isActiveText = "inactive";
+			// var isActiveClasses = "activeness-indicator";
+			// if(period && period.active){
+			// 	isActiveText = "active";
+			// 	isActiveClasses = "activeness-indicator active";
+			// }
 
-		var isActiveText = "inactive";
-		var isActiveClasses = "activeness-indicator";
-		if(this.state.period && this.state.period.active){
-			isActiveText = "active";
-			isActiveClasses = "activeness-indicator active";
-		}
+			ret = (
+				<div>
 
-		return (
-			<div>
+					<div className="frame-input-wrapper required">
+						<label className="container">
+							Name
+							<Input
+								type="text"
+								name="name"
+								placeholder=" "
+								value={this.state.current.valueName}
+								onChange={this.onChangeName.bind(this)}
+							/>
+						</label>
+					</div>
 
-				<div className="frame-input-wrapper required">
-					<label className="container">
-						Name
-						<Input
-							type="text"
-							name="name"
-							placeholder=" "
-							value={this.state.valueName}
-							onChange={this.onChangeName.bind(this)}
-						/>
-					</label>
+					{saveButton}
+
 				</div>
+			);
 
-				{saveButton}
-
-			</div>
-		);
-
+		}
+		return ret;
 	}
 }
 
