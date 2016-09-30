6b40df71befa017ccb709685b5d916b3c3586a95
tmdmc <tmdmc@users.noreply.github.com>
2016-09-30 15:34:16 +0200

    LoginPage update

--- src/components/pages/LoginPage/LoginPage.js
+++ src/components/pages/LoginPage/LoginPage.js
@@ -16,8 +16,8 @@ class LoginPage extends Component {
 
 		this.state = {
 			valueName: "",
-			valuePassword: "",
-			error: "",
+			valuePassphrase: "",
+			errorMessage: "",
 			ready: false
 		};
 	}
@@ -26,17 +26,27 @@ class LoginPage extends Component {
 		onSetTitle: PropTypes.func.isRequired
 	};
 
+	componentDidMount() {
+		this.setState({
+			ready: true
+		});
+	}
+
 	onClick() {
-		this.setState({error: ''});
+		this.setState({
+			errorMessage: ''
+		});
 		let id = utils.guid();
 		ActionCreator.addOperation(id, {});
-		login(this.state.valueName, this.state.valuePassword, this.requestEnd.bind(this, id));
+		login(this.state.valueName, this.state.valuePassphrase, this.requestEnd.bind(this, id));
 	}
 
 	requestEnd(operationId, result) {
 		ActionCreator.removeOperation(operationId);
 		if(result.err) {
-			this.setState({error: "Invalid login information"});
+			this.setState({
+				errorMessage: "Invalid credentials"
+			});
 		} else {
 			Location.pushState(
 				null, publicPath + "/"
@@ -44,51 +54,89 @@ class LoginPage extends Component {
 		}
 	}
 
-	nameChanged(event){
-		this.setState({valueName: event.target.value});
+	onChangeName(event){
+		this.setState({
+			valueName: event.target.value
+		});
 	}
 
-	passwordChanged(event){
-		this.setState({valuePassword: event.target.value});
+	onChangePassphrase(event){
+		this.setState({
+			valuePassphrase: event.target.value
+		});
 	}
 
 	render() {
 		const title = 'Log In';
 		this.context.onSetTitle(title);
+		let loginBoxInsert = null, errorInsert = null;
+
+		if (this.state.errorMessage) {
+			errorInsert = (
+				<div className="login-box-error error">
+					<div className="login-box-error-message">
+						{this.state.errorMessage}
+					</div>
+				</div>
+			);
+		} else {
+			errorInsert = (
+				<div className="login-box-error">
+					<div className="login-box-error-message"></div>
+				</div>
+			);
+		}
+
+		if (this.state.ready) {
+
+			loginBoxInsert = (
+				<div className="login-box frame-wrapper filled">
+					<div className="login-box-content">
+						<label className="container">
+							User
+							<Input
+								type="text"
+								name="name"
+								placeholder=" "
+								value={this.state.valueName}
+								onChange={this.onChangeName.bind(this)}
+							/>
+						</label>
+						<label className="container">
+							Passphrase
+							<Input
+								type="password"
+								name="name"
+								placeholder=" "
+								value={this.state.valuePassphrase}
+								onChange={this.onChangePassphrase.bind(this)}
+							/>
+						</label>
+						<Button
+							basic
+							color="blue"
+							onClick={this.onClick.bind(this)}
+						>
+							Log in
+						</Button>
+					</div>
+					{errorInsert}
+				</div>
+			);
+
+		} else {
+
+			loginBoxInsert = (
+				<div className="login-box frame-wrapper filled loading">
+
+				</div>
+			);
+
+		}
 		return (
 			<div className="full-viewport login-page">
-				<div className="login-box frame-wrapper filled">
-					<label className="container">
-						User
-						<Input
-							type="text"
-							name="name"
-							placeholder=" "
-							value={this.state.valueName}
-							onChange={this.nameChanged.bind(this)}
-						/>
-					</label>
-					<label className="container">
-						Passphrase
-						<Input
-							type="password"
-							name="name"
-							placeholder=" "
-							value={this.state.valuePassword}
-							onChange={this.passwordChanged.bind(this)}
-						/>
-					</label>
-					<label className="container">
-						{this.state.error}
-					</label>
-					<Button
-						basic
-						color="blue"
-						onClick={this.onClick.bind(this)}
-					>
-						Log in
-					</Button>
-				</div>
+				<div className="login-page-loader"></div>
+				{loginBoxInsert}
 			</div>
 		);
 	}
