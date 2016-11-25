ab9cfe7078ce598549e28a33bd11bb9abb88220c
jveselka <jonas.jonas.jonas@gmail.com>
2016-11-25 11:19:08 +0100

    Help: Video Tutorial - added Styles

---
+++ src/components/screens/help/ScreenHelpTutorial11Styles/ScreenHelpTutorial11Styles.js
@@ -0,0 +1,59 @@
+import React, { PropTypes, Component } from 'react';
+
+import ActionCreator from '../../../../actions/ActionCreator';
+
+import logger from '../../../../core/Logger';
+import PantherComponent from "../../../common/PantherComponent";
+
+class ScreenHelpTutorial11Styles extends PantherComponent {
+
+	static contextTypes = {
+		onInteraction: PropTypes.func.isRequired,
+		screenSetKey: PropTypes.string.isRequired
+	};
+
+	onHelpLinkClick(helpScreenKey) {
+		var screenName = this.props.screenKey + "-ScreenHelp" + helpScreenKey;
+		let component = null;
+		let size = 50;
+		switch (helpScreenKey) {
+
+		}
+		if (component) {
+			let options = {
+				component: component,
+				parentUrl: this.props.parentUrl,
+				size: size
+			};
+			ActionCreator.createOpenScreen(screenName, this.context.screenSetKey, options);
+		} else {
+			logger.error("ScreenHelpTutorial11Styles# onHelpLinkClick(), Unknown help screen.", helpScreenKey);
+		}
+	}
+
+	render() {
+
+		return (
+			<div>
+				<div className="screen-content-only help-page"><div>
+					<h2>Video Tutorial 11: Styles</h2>
+
+					<p>Cartographic styles (symbologies) of Layers can be loaded from GeoServer or defined directly in Back Office.</p>
+
+					<iframe
+						className="help-video"
+						width="720"
+						height="421"
+						src="https://www.youtube.com/embed/RKAZ7FlkmVI?rel=0&cc_load_policy=1&cc_lang_pref=en"
+						frameBorder="0"
+						allowFullScreen
+					></iframe>
+
+				</div></div>
+			</div>
+		);
+
+	}
+}
+
+export default ScreenHelpTutorial11Styles;
