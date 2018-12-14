b849503e02db0b4afa894d5daaf920077cd60ef3
Jakub Balhar <jakub@balhar.net>
2018-12-14 12:08:52 +0100

    Remove unused from the Group Model.

--- src/models/GroupModel.js
+++ src/models/GroupModel.js
@@ -18,32 +18,6 @@ class GroupModel extends Model {
 				serverName: 'name',
 				sendToServer: true
 			},
-			changed: {
-				serverName: 'changed', //date
-				sendToServer: false,
-				transformForLocal: this.transformDate
-			},
-			changedBy: {
-				serverName: 'changedBy', //id
-				sendToServer: false,
-				transformForLocal: function (data) {
-					return UserStore.getById(data)
-				},
-				isPromise: true
-			},
-			created: {
-				serverName: 'created', //date
-				sendToServer: false,
-				transformForLocal: this.transformDate
-			},
-			createdBy: {
-				serverName: 'createdBy', //id
-				sendToServer: false,
-				transformForLocal: function (data) {
-					return UserStore.getById(data)
-				},
-				isPromise: true
-			},
 			permissions: {
 				serverName: 'permissions',
 				sendToServer: false
